64426ce0ec461ed04e5a28fa138658f73fd9afc1
ManthanGajjar <manthangajjar93@gmail.com>
2017-12-21 00:10:31 -0800

    basic posees RAM for arm

---
+++ poses.h
@@ -0,0 +1,10 @@
+#ifndef POSES
+#define POSES
+
+#include <avr/pgmspace.h>
+
+PROGMEM prog_uint16_t Center[] = {8, 512, 512, 512, 512, 512, 512, 512, 512};
+PROGMEM prog_uint16_t Home[] = {8, 512, 256, 768, 256, 768, 512, 512, 512};
+
+
+#endif
